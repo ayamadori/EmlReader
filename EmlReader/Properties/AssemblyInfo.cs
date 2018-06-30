340570f7e15e36a50862a851d93b479ef4f6ecc4
ayamadori <ayamadori@gmail.com>
2018-06-30 14:20:58 +0900

    Version bump to 1.2.3

--- EmlReader/Properties/AssemblyInfo.cs
+++ EmlReader/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EmlReader")]
-[assembly: AssemblyCopyright("Copyright © 2017 Ayamadori")]
+[assembly: AssemblyCopyright("Copyright © 2018 Ayamadori")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.2.0")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
 [assembly: ComVisible(false)]
